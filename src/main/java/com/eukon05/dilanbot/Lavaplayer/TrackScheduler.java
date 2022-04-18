51aa8b84755f7bd07b6b9c0b4e07899344ab005a
Łukasz Konieczny <lukimaluki@gmail.com>
2022-04-18 15:19:59 +0200

    Added a "queue" command

--- src/main/java/com/eukon05/dilanbot/Lavaplayer/TrackScheduler.java
+++ src/main/java/com/eukon05/dilanbot/Lavaplayer/TrackScheduler.java
@@ -6,17 +6,14 @@ import com.sedmelluq.discord.lavaplayer.track.AudioTrack;
 import com.sedmelluq.discord.lavaplayer.track.AudioTrackEndReason;
 import lombok.Getter;
 
-import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.LinkedBlockingQueue;
+import java.util.ArrayList;
 
 //Code taken from Lavaplayer's samples, thanks!
-
-
 public class TrackScheduler extends AudioEventAdapter {
     private final AudioPlayer player;
 
     @Getter
-    private final BlockingQueue<AudioTrack> queue;
+    private final ArrayList<AudioTrack> queue;
 
     public AudioTrack loopTrack = null;
 
@@ -25,7 +22,7 @@ public class TrackScheduler extends AudioEventAdapter {
      */
     public TrackScheduler(AudioPlayer player) {
         this.player = player;
-        this.queue = new LinkedBlockingQueue<>();
+        this.queue = new ArrayList<>();
     }
 
     /**
@@ -34,11 +31,8 @@ public class TrackScheduler extends AudioEventAdapter {
      * @param track The track to play or add to queue.
      */
     public void queue(AudioTrack track) {
-        // Calling startTrack with the noInterrupt set to true will start the track only if nothing is currently playing. If
-        // something is playing, it returns false and does nothing. In that case the player was already playing so this
-        // track goes to the queue instead.
         if (!player.startTrack(track, true)) {
-            queue.offer(track);
+            queue.add(track);
         }
     }
 
@@ -46,14 +40,15 @@ public class TrackScheduler extends AudioEventAdapter {
      * Start the next track, stopping the current one if it is playing.
      */
     public void nextTrack() {
-        // Start the next track, regardless of if something is already playing or not. In case queue was empty, we are
-        // giving null to startTrack, which is a valid argument and will simply stop the player.
-        player.startTrack(queue.poll(), false);
+        if(queue.size()==0)
+            return;
+
+        player.startTrack(queue.get(0), false);
+        queue.remove(0);
     }
 
     @Override
     public void onTrackEnd(AudioPlayer player, AudioTrack track, AudioTrackEndReason endReason) {
-        // Only start the next track if the end reason is suitable for it (FINISHED or LOAD_FAILED)
         if (endReason.mayStartNext) {
 
             if(loopTrack==null)
@@ -62,7 +57,6 @@ public class TrackScheduler extends AudioEventAdapter {
                 loopTrack=loopTrack.makeClone();
                 player.startTrack(loopTrack, true);
             }
-
         }
     }
 
